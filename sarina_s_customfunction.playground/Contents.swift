eef74137369c25c412e8c86a8839d123acc5f7db
sarinashepard <67968973+sarinashepard@users.noreply.github.com>
2020-08-04 17:53:18 -0500

    Add files via upload

---
+++ sarina_s_customfunction.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello playground"
+
+var a = 12
+var b = 23
+
+func plus(a: Int, b: Int) -> Int {
+   let a = 12
+    let b = 23
+    return a + b
+}
+print(plus(a:a, b:b))
+
